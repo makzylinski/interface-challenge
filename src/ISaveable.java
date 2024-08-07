c92fc1a3d98365d6c89b135d01fc23ae3b9f4292
Maks <makzylinski@gmail.com>
2024-08-07 14:32:05 +0200

    added interface and Player class

---
+++ src/ISaveable.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface ISaveable {
+    List<String> wirte();
+
+   void read(List<String> list);
+}
---
+++ src/Player.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Player implements ISaveable {
+    private String name;
+    private String weapon;
+    private int hitPoints;
+    private int strength;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    @Override
+    public List<String> wirte() {
+        List<String> list = List.of(name, weapon, Integer.toString(hitPoints), Integer.toString(strength));
+        return null;
+    }
+
+    @Override
+    public void read(List<String> list) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", weapon='" + weapon + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+}
